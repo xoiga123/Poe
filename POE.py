c808235488b9db620b2b5e830ec6d8d88a4ae103
Ngo Viet Hoai Bao <xoiga123@outlook.com.vn>
2023-03-26 22:01:30 +0700

    threading them up

--- POE.py
+++ POE.py
@@ -22,7 +22,7 @@ def set_proxy(proxy):
 def set_auth(key, value):
     headers[key] = value
 
-def load_chat_id_map(bot="a2"):
+def load_chat_id_map(bot="a2", formkey=None, cookie=None, proxies=None):
     data = {
         'operationName': 'ChatViewQuery',
         'query': 'query ChatViewQuery($bot: String!) {\n  chatOfBot(bot: $bot) {\n    __typename\n    ...ChatFragment\n  }\n}\nfragment ChatFragment on Chat {\n  __typename\n  id\n  chatId\n  defaultBotNickname\n  shouldShowDisclaimer\n}',
@@ -33,7 +33,7 @@ def load_chat_id_map(bot="a2"):
     response = requests.post(url, headers=headers, json=data, proxies=proxies)
     return response.json()['data']['chatOfBot']['chatId']
 
-def send_message(message,bot="a2",chat_id=""):
+def send_message(message,bot="a2",chat_id="", formkey=None, cookie=None, proxies=None):
     data = {
     "operationName": "AddHumanMessageMutation",
     "query": "mutation AddHumanMessageMutation($chatId: BigInt!, $bot: String!, $query: String!, $source: MessageSource, $withChatBreak: Boolean! = false) {\n  messageCreate(\n    chatId: $chatId\n    bot: $bot\n    query: $query\n    source: $source\n    withChatBreak: $withChatBreak\n  ) {\n    __typename\n    message {\n      __typename\n      ...MessageFragment\n      chat {\n        __typename\n        id\n        shouldShowDisclaimer\n      }\n    }\n    chatBreak {\n      __typename\n      ...MessageFragment\n    }\n  }\n}\nfragment MessageFragment on Message {\n  id\n  __typename\n  messageId\n  text\n  linkifiedText\n  authorNickname\n  state\n  vote\n  voteReason\n  creationTime\n  suggestedReplies\n}",
@@ -47,7 +47,7 @@ def send_message(message,bot="a2",chat_id=""):
 }
     _ = requests.post(url, headers=headers, json=data, proxies=proxies)
 
-def clear_context(chatid):
+def clear_context(chatid, formkey=None, cookie=None, proxies=None):
     data = {
         "operationName": "AddMessageBreakMutation",
         "query": "mutation AddMessageBreakMutation($chatId: BigInt!) {\n  messageBreakCreate(chatId: $chatId) {\n    __typename\n    message {\n      __typename\n      ...MessageFragment\n    }\n  }\n}\nfragment MessageFragment on Message {\n  id\n  __typename\n  messageId\n  text\n  linkifiedText\n  authorNickname\n  state\n  vote\n  voteReason\n  creationTime\n  suggestedReplies\n}",
@@ -57,7 +57,7 @@ def clear_context(chatid):
     }    
     _ = requests.post(url, headers=headers, json=data, proxies=proxies)
 
-def get_latest_message(bot, retry=5):
+def get_latest_message(bot, retry=5, formkey=None, cookie=None, proxies=None):
     data = {
         "operationName": "ChatPaginationQuery",
         "query": "query ChatPaginationQuery($bot: String!, $before: String, $last: Int! = 10) {\n  chatOfBot(bot: $bot) {\n    id\n    __typename\n    messagesConnection(before: $before, last: $last) {\n      __typename\n      pageInfo {\n        __typename\n        hasPreviousPage\n      }\n      edges {\n        __typename\n        node {\n          __typename\n          ...MessageFragment\n        }\n      }\n    }\n  }\n}\nfragment MessageFragment on Message {\n  id\n  __typename\n  messageId\n  text\n  linkifiedText\n  authorNickname\n  state\n  vote\n  voteReason\n  creationTime\n}",
